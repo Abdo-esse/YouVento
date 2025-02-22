4d887fa68e870c3deb2a28478a96596baff18ba2
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-22 14:50:27 +0100

    J'ai termin la select de categories

---
+++ project/app/Http/Controllers/CategorieController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategorieRequest;
+use App\Http\Requests\UpdateCategorieRequest;
+use App\Models\Categorie;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories= Categorie::query()->paginate(1);
+        return view('categorie.index',compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categorie=new Categorie();
+        return view('categorie.create',compact('categorie'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCategorieRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategorieRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCategorieRequest  $request
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategorieRequest $request, Categorie $categorie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Categorie  $categorie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Categorie $categorie)
+    {
+        //
+    }
+}
--- project/resources/views/categorie/create.blade.php
+++ project/resources/views/categorie/create.blade.php
@@ -1,7 +1,10 @@
 
+<x-master title="Categories" >
+@section('main')
+
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="pb-4 bg-white dark:bg-gray-900">
+    <div class="flex justify-between  gap-y-8 pb-4 bg-white dark:bg-gray-900">
         <label for="table-search" class="sr-only">Search</label>
         <div class="relative mt-1">
             <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -11,10 +14,19 @@
             </div>
             <input type="text" id="table-search" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
         </div>
+        <div class="relative mt-1">
+            <a href="" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            <button>Add Categorie</button>
+            </a>
+           
+        </div>
     </div>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
+                <th scope="col" class="px-6 py-3">
+                    id
+                </th>
                 <th scope="col" class="px-6 py-3">
                     Categories
                 </th>
@@ -30,21 +42,34 @@
             </tr>
         </thead>
         <tbody>
+            @forelse($categories as $categorie)
             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
+                    {{$categorie->id}}
                 </th>
                 <td class="px-6 py-4">
-                    Silver
+                {{$categorie->name}}
                 </td>
                 <td class="px-6 py-4">
-                    Laptop
+                {{$categorie->created_at}}
+                </td>
+                <td class="px-6 py-4">
+                {{$categorie->updated_at}}
                 </td>
                 <td class="flex gap-x-4 px-6 py-4">
                     <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                     <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</a>
                 </td>
             </tr>
+            @empty
+            <tr>
+                <td><h2>not Categories</h2></td>
+            </tr>
+            
+            @endforelse
         </tbody>
     </table>
 </div>
+
+@endsection
+</x-master>
