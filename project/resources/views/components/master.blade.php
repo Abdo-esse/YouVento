2d2f12846b942b8840aa3dbd7a29da3d0f417bf3
abdelilah2005 <abdelilahessemlali@gmail.com>
2025-02-22 11:31:22 +0100

    J'ai termin la commponent siedbar du Admin

---
+++ project/resources/views/components/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title }}</title>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <style type="text/tailwindcss">
+      @theme {
+        --color-clifford: #da373d;
+      }
+    </style>
+
+</head>
+<body>
+       @include('partials.siedbar')
+       <main class="p-4 sm:ml-64">
+        @yield('main')
+       </main>
+</body>
+</html>
